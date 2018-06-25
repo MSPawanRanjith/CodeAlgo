180efbea4b5d9ac7ad126d85e9e07c1f537f3abc
M S Pawan Ranjith <pawanranjith@gmail.com>
2018-06-25 10:39:29 +0530

    stack method

--- PrintBracket.py
+++ PrintBracket.py
@@ -8,13 +8,12 @@ Lang :  python
 for r in range(0,int(input())):
     exp=input();
     i=1;
+    list=[]
     for ch in exp:
         #print(ch)
         if ch=='(':
-            print(i,end =" ");
+            print(i,end=" ");
             i=i+1;
         elif ch==')':
-            i=i-1;
-            print(i,end =" ");
-            i=i+1;
+            print(list.pop(-1), end=" ")
     print("")

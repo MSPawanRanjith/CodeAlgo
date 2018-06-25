4a90a3e376f064ecb7c22ab5d09a147ad96cec27
M S Pawan Ranjith <pawanranjith@gmail.com>
2018-06-25 10:37:00 +0530

    Print baracket

---
+++ PrintBracket.py
@@ -0,0 +1,20 @@
+"""Print Bracket Number :GFG 
+Difficulty : easy
+Lang :  python
+
+Answer :"""
+
+
+for r in range(0,int(input())):
+    exp=input();
+    i=1;
+    for ch in exp:
+        #print(ch)
+        if ch=='(':
+            print(i,end =" ");
+            i=i+1;
+        elif ch==')':
+            i=i-1;
+            print(i,end =" ");
+            i=i+1;
+    print("")
